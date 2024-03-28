dd6ebf664ff90627c7aa8924a9f7ba3a273cf29d
aldbr <aldbr@outlook.com>
2024-03-28 17:40:53 +0100

    feat: add cypress in the CI

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     specPattern: "test/e2e/**/*.cy.ts",
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
