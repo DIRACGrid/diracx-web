b51fb3a403fa451f85a271cb23baaa69f16bb0eb
aldbr <aldbr@outlook.com>
2024-11-04 16:04:05 +0100

    feat(CI): add extension tests within the CI

--- packages/diracx-web/next.config.js
+++ packages/diracx-web/next.config.js
@@ -1,19 +1,25 @@
-const path = require("path");
+import path from "path";
+import { fileURLToPath } from "url";
+import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  transpilePackages: ["@dirac-grid/diracx-web-components"],
-  webpack: (config) => {
-    config.resolve.alias["@dirac-grid/diracx-web-components"] = path.resolve(
-      __dirname,
-      "../diracx-web-components/src",
-    );
-    return config;
-  },
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+
+  return {
+    transpilePackages: ["@dirac-grid/diracx-web-components"],
+    ...(isDev && {
+      webpack: (config) => {
+        config.resolve.alias["@dirac-grid/diracx-web-components"] =
+          path.resolve(__dirname, "../diracx-web-components/src");
+        return config;
+      },
+    }),
+    output: "export",
+    images: {
+      unoptimized: true,
+    },
+  };
 };
-
-module.exports = nextConfig;
